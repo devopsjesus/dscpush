a1e55034538832e62c849153c3bb79a393675d04
Jason Ryberg <jaryber@microsoft.com>
2018-05-24 20:18:39 -0700

    adding workshop deployment wrapper adding Pester Tests to ensure VMs configs are applied

---
+++ DeployWorkshop.Tests.ps1
@@ -0,0 +1,42 @@
+﻿#requires -RunAsAdministrator
+#requires -Module Pester
+
+<#
+    .Synopsis
+        Run these tests after deploying the DscPush Workshop configurations to infrastructure. 
+        The VMs will need to reboot a few times before the Test-DscConfiguration cmdlet comes back true.
+
+    .Parameter Credential
+        The administrator credential for your image.
+
+    .Parameter vmNetworkAddressList
+        The IP addresses of the VMs you built.  Hopefully with code...
+#>
+param(
+    $Credential = (New-Object System.Management.Automation.PSCredential (“administrator”, (ConvertTo-SecureString "P@ssw0rd123" -AsPlainText -Force))),
+
+    $vmNetworkAddressList = @("192.0.0.236","192.0.0.237")
+)
+
+Describe "DscPush Workshop Deployment" {
+    Context "VM DSC State" {
+        
+        #region Prepare for test
+        $cimSessions = New-CimSession -ComputerName $vmNetworkAddressList -Credential $Credential
+        Stop-DscConfiguration -CimSession $cimSessions -WarningAction Ignore
+        #endregion
+
+        It "Start-DscConfiguration should run successfully on the VMs" {
+            { Start-DscConfiguration -CimSession $cimSessions -Wait -UseExisting } | Should Not Throw
+        }
+        
+        It "Test-DscConfiguration should return True for both VMs" {
+            $testDscReturn = Test-DscConfiguration -CimSession $cimSessions
+            $testDscReturn | Should Be @($True,$True)
+        }
+
+        It "Get-DscConfiguration should run successfully on the VMs" {
+            { Get-DscConfiguration -CimSession $cimSessions } | Should Not Throw
+        }
+    }
+}
---
+++ DeployWorkshop.ps1
@@ -0,0 +1,32 @@
+﻿#requires -RunAsAdministrator 
+#requires -Module Pester
+
+param(
+    $Credential = (New-Object System.Management.Automation.PSCredential (“administrator”, (ConvertTo-SecureString "P@ssw0rd123" -AsPlainText -Force))),
+
+    $vmNetworkAddressList = @("192.0.0.236","192.0.0.237"),
+
+    [switch]
+    $ClobberVMs
+)
+
+#cleanup
+Remove-Item C:\DscPushWorkshop,C:\Windows\Temp\dscpushworkshop -Recurse -Force -ErrorAction Ignore
+
+if ($ClobberVMs)
+{
+    Stop-VM dscpushdc,dscpushch -Force -ErrorAction Ignore
+    Remove-VM dscpushdc,dscpushch -Force -ErrorAction Ignore
+    Remove-Item 'C:\users\Public\Documents\Hyper-V\Virtual hard disks\DscPushCH.vhdx','C:\users\Public\Documents\Hyper-V\Virtual hard disks\DscPushDC.vhdx' -ErrorAction Ignore
+}
+
+Start-BitsTransfer -Source https://github.com/devopsjesus/dscpush/archive/master.zip -Destination C:\windows\Temp\dscpushworkshop.zip
+Expand-Archive C:\windows\Temp\dscpushworkshop.zip -DestinationPath C:\windows\Temp\dscpushworkshop -Force
+$null = New-Item C:\DSCPushWorkshop -ItemType Directory -Force
+Copy-Item C:\Windows\Temp\dscpushworkshop\dscpush-master\* -Destination C:\DSCPushWorkshop -Force
+
+. C:\DSCPushWorkshop\WorkshopStep0-hyperv.ps1 -clobber -Credential $Credential
+
+. C:\DSCPushWorkshop\WorkshopStep1.ps1
+
+. C:\DSCPushWorkshop\WorkshopStep2 -DeploymentCredential $Credential
